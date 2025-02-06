6bcb0f54d387d73736ea09d69755964e7181d414
eltatataa <davidtowo75@gmail.com>
2025-02-06 18:53:58 -0500

    order service: new name property on items

--- order-service/src/config/schemas/order-dtos.schema.ts
+++ order-service/src/config/schemas/order-dtos.schema.ts
@@ -2,19 +2,22 @@ import { z } from 'zod';
 import { CreateOrderDto, UpdateOrderDto } from '../../domain';
 
 export const createOrderDtoSchema: z.ZodType<CreateOrderDto> = z.object({
-  userId: z.string(),
-  items: z.array(
-    z.object({
-      productId: z.string(),
-      quantity: z.number(),
-      price: z.number(),
-    }),
-  ),
-  address: z.string(),
+  userId: z.string().trim().nonempty("User ID can't be empty"),
+  items: z
+    .array(
+      z.object({
+        productId: z.string(),
+        name: z.string(),
+        quantity: z.number(),
+        price: z.number(),
+      }),
+    )
+    .min(1, 'Items must have at least one product'),
+  address: z.string().trim().nonempty("Address can't be empty"),
 });
 
 export const updateOrderDtoSchema: z.ZodType<UpdateOrderDto> = z.object({
-  id: z.string(),
+  id: z.string().trim().nonempty("Order ID can't be empty"),
   status: z.enum([
     'pending',
     'processing',
--- order-service/src/data/mongo/models/order.model.ts
+++ order-service/src/data/mongo/models/order.model.ts
@@ -7,6 +7,7 @@ const orderSchema = new mongoose.Schema(
       {
         _id: false,
         productId: { type: String, required: true },
+        name: { type: String, required: true },
         quantity: { type: Number, required: true },
         price: { type: Number, required: true },
       },
